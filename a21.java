4550d85951193736f3505c93d5b1a28cc3778f1f
GANGA C K <kochupotty@gmail.com>
2016-11-03 20:57:44 +0530

    assignment 2-1

---
+++ a21.java
@@ -0,0 +1,12 @@
+package a2;
+
+public class a21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(System.out.printf("GANGA C K")!=null){
+	    	 
+	     }
+	}
+
+}
